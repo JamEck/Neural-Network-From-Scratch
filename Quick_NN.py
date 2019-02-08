ef38bbf83aa021565a42de755cc24562a095b217
JamEck <james.a.eckdahl@gmail.com>
2019-02-07 21:02:53 -0800

    Neural Net from Scratch
    Complete rewrite. created NN class

--- Quick_NN.py
+++ Quick_NN.py
@@ -1,313 +1,248 @@
-####### QUICK, BASIC NEURAL NETWORK EXERCISE #######
-#######    WRITTEN FROM SCRATCH IN PYTHON    #######
-
-####### IMPORT PACKAGES #######
-
 import numpy as np
-import random as rand
 import matplotlib.pyplot as plt
 
-
-#### FOR LOADING IN MNIST IMAGES FROM FILE ####
-def loadMNISTimages(fileName):
-    with open(fileName, 'br') as file:
-    
-        file.seek(4)       # seek ahead to skip magic number
-        buf = file.read(4) # read next 4 bytes into a buffer
-
-        # convert those bytes into an integer
-        # imgNum = number of images in file
-        imgNum  = int.from_bytes(buf,"big") # interpret bytes as big-endian (MSB first)
-
-        buf = file.read(4)
-        imgHeight = int.from_bytes(buf,"big")
-
-        buf = file.read(4)
-        imgWidth  = int.from_bytes(buf,"big")
-
-        imgs = [[]]*imgNum # list to hold all images
-
-        # all images in the MNIST Data Set are 28x28 grayscale images
-        # and are stored consecutively after the file header
-
-        for i in range(imgNum):
-            buf = file.read(imgWidth*imgHeight) # read in bytes for i'th image
-            imgs[i] = np.asarray(list(buf)).reshape(28,28) # convert to 28x28 np array
-    
-    return imgs
-
-
-#### FOR LOADING IN MNIST LABELS FROM FILE ####
-def loadMNISTlabels(fileName):
-    with open(fileName, 'br') as file:
-
-        file.seek(4) # skip magic number
-
-        buf = file.read(4)
-        labNum = int.from_bytes(buf,"big") # number of labels
-
-        labels = np.zeros(labNum, dtype = np.uint8) # preallocate array to hold labels
-        buf = file.read(labNum) # read in labels
-
-        # convert each label from a byte to an integer and pass each to array
-        for i, byte in enumerate(buf):
-            labels[i] = int(byte)
-
-        file.close()
-
-    return labels
-
-
-#### SIGMOID FUNCTION ####
-def sig(x):
-    return 1/(1 + pow(2.718, -x))
-
-
-#### DERIVATE OF SIGMOID FUNCTION ####
-def dsig(x):
-    ans  = pow(2.718, x/2)
-    ans += 1/ans
-    ans *= ans
-    ans  = 1/ans
-    return ans
-
-
-#### PASS INPUT THROUGH NN AND GET OUTPUT ####
-
-def runNet(inp):
-    layer[0] = np.expand_dims(inp.flatten(),axis=1)/255
-    for i,each in enumerate(weightMat):
-        layer[i+1] = each.dot(layer[i])
-        layer[i+1] = sig(layer[i+1])
-    return layer[-1]
-
-
-#### GET DESIRED OUTPUT OF NN USING LABEL ####
-
-def getY(label):
-    y = np.zeros(layer[-1].shape)
-    y[label] = 1;
-    return y
-
-
-#### FIND LOSS FOR PARTICULAR OUTPUT ####
-
-def getCost(label):
-    y = getY(label)
-    d = (y - layer[-1])
-    return (d*d).sum()
-
-
-#### CLASSIFY AN INPUT USING THE CURRENT WEIGHTS ####
-
-def classify(inp):
-    runNet(inp)
-    ans = layer[-1].argmax()
-    return ans
-
-
-#### DERIVATIVE OF ACTIVATED LAYER WRT TO UNACTIVATED LAYER ####
-
-def buildSigmoidSensitivityMatrix(n):
-    S = layer[n]*(1-layer[n])
-    S = S.dot(np.ones((1,S.shape[0])))
-    S*=np.eye(S.shape[0])
-    
-    return S
-
-
-#### DERIVATIVE OF ONE LAYER RELATIVE TO PREVIOUS LAYER ####
-
-def getLayerTransition(n):
-    if(n == 0):
-        print("Cannot perform on input layer!")
-        return
-    
-    pos = n >= 0
-    
-    S = buildSigmoidSensitivityMatrix(n)
-
-    res = weightMat[n - pos].transpose().dot(S)
-    return res
-
-
-#### DERIVATIVE OF UNACTIVATED LAYER WRT PREVIOUS WEIGHT MATRIX ####
-
-def buildBetaOmegaSensitivityMatrix(n): 
-    if(n == 0):
-        print("Cannot perform on input layer!")
-        return
-    pos = n >= 0
-    
-    s = weightMat[n-pos].shape    
-    ans = np.zeros((1,s[0],s[1]))
-    ans = np.pad(ans,((0,s[0]-1),(0,0),(0,0)), "edge")
-    for k,each in enumerate(ans):
-        each[k] = layer[n-1].transpose()
-    return ans
-
-
-#### WORKAROUND TO MAKE NUMPY LOOK LIKE MY NOTES :) ####
-
-def convertMatDim(shape, mode=0):
-    if(len(shape) > 2):
-        if (mode == 0):
-            p1 = np.asarray(shape[:-2])
-            p2 = np.asarray(shape[-2:])
-            p1 = np.flip(p1,0)
-        else:
-            p1 = np.asarray(shape[:2])
-            p2 = np.asarray(shape[2:])
-            p2 = np.flip(p2,0)
-        
-        ans = np.concatenate((p2, p1))
-    else:
-        ans = shape
-    return ans
-
-
-#### SETS UP AND ANALYZES MATRICES FOR dot3D ####
-
-def dot3Dsetup(mat1, mat2):
-    ms1 = convertMatDim(mat1.shape)
-    ms2 = convertMatDim(mat2.shape)
-    
-    # concatenate all but last of first with all but first of last
-    ansShape = np.concatenate((ms1[:-1], ms2[1:]))
-    ansShape = np.pad(ansShape, (0,4 - len(ansShape)), "constant", constant_values=1)
-    ansShape = convertMatDim(ansShape,1).astype(int)
-    
-    if(ms1[-1] != ms2[0]):
-        print("Matrices do not conform!")
-        return -1
-    if(len(ms1) > 3 or len(ms2) > 3):
-        print("No more than 3 dimensions!")
-        return -1
-
-    rank1 = len(mat1.shape)
-    rank2 = len(mat2.shape)
-    
-    if(rank2 == 1):
-        rank2 += 1
-        mat2 = np.expand_dims(mat2,1) # make column vector out of mat2
-    
-    for i in range(3-rank1):
-        mat1 = np.expand_dims(mat1,0) # make each a 3D matrix
-    for i in range(3-rank2):
-        mat2 = np.expand_dims(mat2,0)
-    
-    return [ansShape,mat1,mat2,rank1,rank2]
-
-
-#### CUSTOM 3D MATRIX MULTIPLICATION ####
-
-def dot3D(mat1, mat2):  
-    
-    ansShape,mat1,mat2,rank1,rank2 = dot3Dsetup(mat1, mat2)
-    if(ansShape.any() == -1):
-        print("Dot Product Failed :(")
-        return
-    
-    ms1 = mat1.shape
-    ms2 = mat2.shape
-    ans = np.zeros(ansShape)
-    
-    for r1 in range(ms1[1]):
-        for c1 in range(ms1[2]):
-            for l2 in range(ms2[0]):
-                for c2 in range(ms2[2]):
-                    a = mat1[:,r1,c1]
-                    b = mat2[l2,:,c2]
-                    ans[l2,c2,r1,c1] = a.dot(b)
-            
-    while(ans.shape[0] == 1):
-        ans = ans[0]
-        
-    return ans
-
-
-#### PHEW! FINALLY SOME GRADIENT DESCENT ####
-
-def backpropUsing(inp, label, trainStep):
-    y  = getY(label)
-    runNet(inp)
-    dC = y - layer[-1]
-    for n in range(len(layer)-1, 0, -1):
-        S  = buildSigmoidSensitivityMatrix(n)
-        bw = buildBetaOmegaSensitivityMatrix(n)
-        dW = dot3D(bw,S.dot(dC))
-        mag = np.linalg.norm(dW)
-        if(mag != 0):
-            dW /= mag
-        else:
-            dW /= 0.000001
-        dW *= trainStep
-        weightMat[n-1] += dW
-        dC  = getLayerTransition(n).dot(dC)
-
-
-
-
-###############################
-########## MAIN CODE ##########
-###############################
-
+import MNIST_Loader as ml
 
 ####### LOAD DATA SET #######
 
-trainImg = loadMNISTimages("train-images-idx3-ubyte")
-testImg  = loadMNISTimages("t10k-images-idx3-ubyte" )
-trainLab = loadMNISTlabels("train-labels-idx1-ubyte")
-testLab  = loadMNISTlabels("t10k-labels-idx1-ubyte")
+trainImg = ml.loadMNISTimages("train-images-idx3-ubyte").reshape((-1,784))/255
+testImg  = ml.loadMNISTimages("t10k-images-idx3-ubyte" ).reshape((-1,784))/255
+trainLab = ml.loadMNISTlabels("train-labels-idx1-ubyte")
+testLab  = ml.loadMNISTlabels("t10k-labels-idx1-ubyte")
 
-# 60,000 training images
-# 60,000 training labels
-# 10,000 test images
-# 10,000 test labels
+# testImg  (10000, 784)
+# testLab  (10000)
+# trainImg (60000, 784)
+# trainLab (60000)
+
+class NN:
+    def __init__(self,sizes):
+        self.sizes = sizes.copy()
+        self.depth = len(list(sizes))-1 # number of layers, excluding the output layer
+        
+        # Create Layers and Parameters
+        self.layer     = [[]]*self.depth
+        self.beta      = [[]]*self.depth
+        self.weightMat = [[]]*self.depth
+        self.bias      = [[]]*self.depth
+        for i in range(self.depth):
+            self.layer[i] = np.ones(sizes[i])
+            self.beta[i]  = np.ones(sizes[i])
+            self.weightMat[i] = 2*np.random.random((sizes[i], sizes[i+1]))-1
+            self.bias[i]      = 2*np.random.random(sizes[i+1])-1
+        
+        # list of parameter gradients for backpropagation
+        self.wgrad = [np.zeros(self.weightMat[i].shape) for i in range(self.depth)]
+        self.bgrad = [np.zeros(self.bias[i].shape) for i in range(self.depth)]
+        
+        # Create Layers and Parameters
+        self.output = np.ones(sizes[-1])
+        self.betaOutput = np.ones(sizes[-1])
+        
+        self.activation = NN.sigmoid
+        self.activationDeriv = NN.dsigmoid
+        
+        self.costFn = self.MSE
+        self.dcostFn = self.dMSE
+        self.cost = 0
+
+    def prepInp(self, inp):
+        try:
+            inp = np.array(inp,dtype=np.float64)
+        except:
+            print("Could not convert input into numpy array.")
+            return
+        
+        if(inp.ndim == 0):
+            inp = np.expand_dims(inp,0)
+              
+        if(inp.shape[0]!=self.sizes[0]):
+            print("Dimensions don't conform to NN input layer.")
+            print("Input Entered:", inp.shape, "\nInput Layer: ({}, 1)".format(self.sizes[0]))
+            return
+        
+        return inp
+    
+    def sigmoid(inp):
+        return 1/(1+2.718**(-inp))
+    def dsigmoid(inp):
+        temp = NN.sigmoid(inp)
+        return temp*(1-temp)
+    
+    def relu(inp):
+        return inp*(inp>0)
+    def drelu(inp):
+        return   1*(inp>0)
+    
+    def tanh(inp):
+        return 2/(1+2.718**(-2*inp))-1
+
+    
+    def forward(self, inp):        
+        try:
+            np.copyto(self.layer[0],self.prepInp(inp))
+            np.copyto(self.beta[0],self.prepInp(inp))
+        except:
+            print("Couldn't copy to input layer.")
+            return
+                
+        for i in range(self.depth-1):
+            self.beta[i+1] = self.layer[i].dot(self.weightMat[i]) + self.bias[i]
+            self.layer[i+1] = self.activation(self.beta[i+1])
+            
+        self.betaOutput = self.layer[-1].dot(self.weightMat[-1]) + self.bias[-1]
+        self.output = self.activation(self.betaOutput)
+        return self.output
+    
+    def getDesired(self, label, val = 1):
+        output = np.zeros(self.output.shape)
+        if(label >= self.output.size or label < 0):
+            print("Label out of range.")
+        else:
+            output[label] = val
+        return output
+        
+    def MSE(self, act, des):
+        return ((des - act)**2).sum()/act.size
+    def dMSE(self, act, des):
+        return 2*(des - act)/act.size
+    
+    def getCost(self, label):
+        self.cost = self.costFn(self.output, self.getDesired(label))
+        return self.cost
+
+    def Build_Activation_Sensitivity_Matrix(self,inp):
+        '''Derivative of an activated layer wrt to its unsquashed layer'''
+        k = inp.shape[0]
+        ans = np.zeros((k,k))
+        for i in range(k):
+            ans[i,i] = self.activationDeriv(inp[i])
+        return ans
+        
+    def Build_Beta_Omega_Sensitivity_Matrix(self, layerNum):
+        '''Derivative of the unsquashed layer wrt to the Weight Matrix'''
+        if(layerNum >= self.depth):
+            print("Index out of range.")
+            return -1
+        
+        [j,k] = self.weightMat[layerNum].shape 
+        ans = np.zeros((k,j,k))
+        for n in range(k):
+            np.copyto(ans[n,:,n],self.layer[layerNum])
+        return ans
+    
+    def backprop(self, label):
+        accum = self.dcostFn(self.output, self.getDesired(label))
+        accum = accum.dot(self.Build_Activation_Sensitivity_Matrix(self.betaOutput))
+        
+        for i in range(self.depth):
+            ind = -(i+1)
+            self.wgrad[ind] += accum.dot(self.Build_Beta_Omega_Sensitivity_Matrix(ind).swapaxes(0,1))
+            self.bgrad[ind] += accum
+            accum = accum.dot(self.weightMat[ind].transpose())
+            accum = accum.dot(self.Build_Activation_Sensitivity_Matrix(self.beta[ind]))
+            
+    def stepSGD(self, r = 0.1):
+        for i,dw in enumerate(self.wgrad):
+            # normalize gradient vector
+            dwmag = np.linalg.norm(dw)
+            if(dwmag!=0):
+                dw/=dwmag
+            self.weightMat[i] += dw*r
+        for i,db in enumerate(self.bgrad):
+            dbmag = np.linalg.norm(db)
+            if(dbmag!=0):
+                db/=dbmag
+            self.bias[i] += db*r
+        
+        for each in self.wgrad:
+            each.fill(0)
+        for each in self.bgrad:
+            each.fill(0)
+
+    def train(self, inp, label, batchSize, r=0.1):
+        batchSize = int(batchSize)
+        if(batchSize<=0):
+            print("Invalid Batch Size. Setting to default of 10")
+            batchSize = 10
+            
+        if(inp.ndim == 1):
+            inp = np.expand_dims(inp, 0)
+        if(label.ndim == 0):
+            label=np.expand_dims(label,0)
+           
+        if(inp.shape[0]!=label.shape[0]):
+            print("Unequal inputs and labels.")
+            return
+        
+        for i in range (inp.shape[0]):
+            self.forward(inp[i])
+            self.backprop(label[i])
+            if(i%batchSize==0):
+                self.stepSGD(r)
+            if(i%(inp.shape[0]/10)==0):
+                print("%d%%" % (i/inp.shape[0]*100), end=" ")
+
+           
+        self.stepSGD(r)
+        print("100%\nFINISHED")
+        
+    def classify(self, inp):
+        self.forward(inp)
+        guess = self.output.argmax()
+        confidence = self.output.max()
+#         print("%d with confidence of %.3f"% (guess, confidence))
+        return [guess, confidence]
+
+    def accuracyTrain(self, imgNums):
+        perdigit = np.zeros(10)
+        digitCount = np.zeros(10)
+        for imgNum in imgNums:
+            guess = self.classify(trainImg[imgNum])[0]
+            lab   = trainLab[imgNum]
+#             print("%d)"%imgNum, guess, lab)
+            if(guess == lab):
+                perdigit[lab] += 1
+            digitCount[lab]+=1
+            
+        return [perdigit/digitCount, perdigit.sum()/imgNums.size*100]
+    
+    def accuracyTest(self, imgNums):
+        perdigit = np.zeros(10)
+        digitCount = np.zeros(10)
+        for imgNum in imgNums:
+            guess = self.classify(testImg[imgNum])[0]
+            lab   = testLab[imgNum]
+#             print("%d)"%imgNum, guess, lab)
+            if(guess == lab):
+                perdigit[lab]+=1
+            digitCount[lab]+=1
+            
+        return [perdigit/digitCount, perdigit.sum()/imgNums.size*100]
 
 
-####### GENERATE LAYERS #######
-structure = [784, 16, 7, 10]
-layer   = [np.zeros((i,1)) for i in structure]
+sizes = [784,16,10,10]
+
+n = NN(sizes)
+n.activation      = NN.sigmoid
+n.activationDeriv = NN.dsigmoid
 
 
-####### GENERATE WEIGHT MATRICES AND INITIALIZE WITH RANDOM VALUES (-1, 1) #######
-weightMat = [2*np.random.random((structure[i+1],structure[i]))-1 for i in range(len(structure) - 1)]
+N = 10000
+epochNum = 3
+for epoch in range(epochNum):
+    r=0.1
+    print("Epoch %d: Learning rate set to %0.2f" % (epoch+1, r))
+    n.train(trainImg[:N], trainLab[:N], 20)
 
 
-####### BASIC SGD #######
-
-sampNum = 1000 # perform sgd on first 'sampNum' images in training data
-
-for imgNum in range(sampNum):
-    backpropUsing(trainImg[imgNum], trainLab[imgNum], 0.4)
+print(n.accuracyTrain(np.arange(10000)))
+print(n.accuracyTest(np.arange(10000)))
 
 
-##### TEST NN ON TRAINING DATA FIRST #####
+imgNum = 1060
+print(n.classify(trainImg[imgNum]))
+print(trainLab[imgNum])
 
-acc = 0
-randAcc = 0
-for each in range(sampNum):
-    rc = np.floor(np.random.rand()*10).astype(int)
-    acc += (classify(trainImg[each]) == trainLab[each])
-    randAcc += (rc == trainLab[each])
+n.output.sum()
 
-print("Results")
-print(acc)
-print("out of", sampNum)
-print(acc/sampNum)
-
-print("\nRandom Control")
-print(randAcc)
-print("out of", sampNum)
-print(randAcc/sampNum)
-
-##### TEST NN ON TEST DATA #####
-
-acc= 0
-for i,lab in enumerate(testLab):
-    acc += (classify(testImg[i]) == lab)
-print("Correct Classifications out of 10000 Data:\n", acc)
-
-print("Accuracy on Test Data:")
-print(acc/len(testLab))
